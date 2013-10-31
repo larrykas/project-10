f0aec406e52ec3958192b824d078636680e4cdf5
larrykas <larrykas@users.noreply.github.com>
2013-10-31 08:45:41 +0300

    Update regex.rb

--- regex.rb
+++ regex.rb
@@ -1,17 +1,37 @@
 # 2.
 # http://www.google.com/a/5    #should return 5
+
+x = 'http://www.google.com/a/5'
+regx = /[0-9]/
+value = x.scan(regx)
+value[0].to_i
+=> 5
+
 # http://www.yahoo.com/a/19   #should return 19
+
+y = 'http://www.yahoo.com/a/19'
+regx = /[0-9]/
+value2 = y.scan(regx)
+value2[0].to_i
+=> 19
 ############################################################################
- [0-9] Regular Expression that selects all digits with in the range of 0-9
-############################################################################
+
 # 3.
 # <div class=”price”>Price: $10</div>  #should return 10 (not $10)
+
+a = '<div class="price">Price: $10</div>'
+val = a.scan(/\d+/)
+val[0].to_i
+=> 10
 # <div class=”price”>Price: $25</div> #should return 25 (not $25)
 
-############################################################################
- [0-9] Regular Expression that selects all digits with in the range of 0-9
-############################################################################ 
+b = '<div class="price">Price: $25</div>'
+val = b.scan(/\d+/)
+val[0].to_i
+=> 25
 
+############################################################################
+ 
 # 4.
 ############################################################################################
 string = "<html>
@@ -22,7 +42,7 @@ string = "<html>
   </body>
  </html>"
 
-emailRegex = /([^\b\s\t\n]*\@[a-z0-9\-\_]*\.[a-z]{2,4})/i
+email_regx = /\w+@\w+.{4}|\w+\s+at\s+\w+.{4}/
 
-string.scan(emailRegex)
+string.scan(email_regx)
 ############################################################################################
